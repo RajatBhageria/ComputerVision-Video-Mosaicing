4cae90b661fe9144f8a85025c3590e4fea51a1ad
Kashish Gupta <kashishg@wharton.upenn.edu>
2017-11-10 21:58:11 -0500

    Fixed feature matching

--- Mosaicing/feat_match.m
+++ Mosaicing/feat_match.m
@@ -15,7 +15,7 @@ function [match] = feat_match(descs1, descs2)
 descs1 = descs1';
 descs2 = descs2';
 
-output = zeros(size(descs1, 1), 1, 'uint16');
+output = zeros(size(descs1, 1), 1);
 
 [idx, d] = knnsearch(descs2, descs1, 'k', 2);
 
